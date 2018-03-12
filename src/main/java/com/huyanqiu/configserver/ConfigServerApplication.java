0e6dc592f58a8524bac9099a655125d4d867b7f5
Bonnie1215 <1224022190@qq.com>
2018-03-12 17:03:56 +0800

    分布式事物

---
+++ src/main/java/com/huyanqiu/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.huyanqiu.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer // 开启Spring Cloud Config的服务端功能
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
