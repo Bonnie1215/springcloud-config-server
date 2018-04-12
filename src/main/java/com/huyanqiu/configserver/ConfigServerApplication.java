a207fad441b8e42a73f1bbe6cd568b05793307fb
Bonnie1215 <1224022190@qq.com>
2018-04-12 17:44:49 +0800

    新增方法

--- src/main/java/com/huyanqiu/configserver/ConfigServerApplication.java
+++ src/main/java/com/huyanqiu/configserver/ConfigServerApplication.java
@@ -9,6 +9,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("1111111111111111111111111");
+		System.out.println("222222222222222222222");
+		System.out.println("33333333333333333333333333333");
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+	
+	public static void a () {
+		System.out.println("本地分支新增方法");
 	}
 }
